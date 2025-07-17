b3cd889e0ae33b6c044901b47914263c21512c92
Fermix <65306075+566hj@users.noreply.github.com>
2025-07-17 20:47:46 +0400

    Delete EventHandlerAttribute.cs

--- EventHandlerAttribute.cs
+++
@@ -1,6 +0,0 @@
-using System;
-[AttributeUsage(AttributeTargets.Method)]
-public class EventHandlerAttribute : Attribute {
-    public string EventName { get; }
-    public EventHandlerAttribute(string name) => EventName = name;
-}
